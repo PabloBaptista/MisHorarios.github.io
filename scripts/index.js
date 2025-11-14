e79d47e7323f1b04bbc7b0a031350c32103d6247
PabloBaptista <146459738+PabloBaptista@users.noreply.github.com>
2025-11-14 14:52:02 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -13,8 +13,8 @@ let calculado=false
 
 
 
-const eventStore=initEventStore();
-initCalendar(eventStore);
+//const eventStore=initEventStore();
+//initCalendar(eventStore);
 initEventCreateButton();
 initEventFormDialog();
 initNav();
@@ -24,3 +24,4 @@ initEventDetailsDialog();
 
 
 getDaysBetweenTwoDatesIncluded(new Date("2025-11-03"),new Date("2025-11-07"));
+
